01da0355102224cff9bbf5b7b474c18fa8c88d3b
Andrey <andr.poluehktov@yandex.ru>
2024-05-09 13:28:45 +0300

    change

--- components/PostCard/PostLink/PostLink.tsx
+++ components/PostCard/PostLink/PostLink.tsx
@@ -1,12 +1,13 @@
 import styles from './PostLink.module.css';
 import ArrowIcon from './arrow.svg';
+import Link from 'next/link';
 
 export default function PostLink() {
 
 	return (
-		<div className={styles.link}>
+		<Link className={styles.link} href={''}>
             Читать
 			<ArrowIcon />
-		</div>
+		</Link>
 	);
 }

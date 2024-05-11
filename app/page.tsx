314f9c9b9d3d9eaf96be0c558a0e464d03e617c8
Andrey <andr.poluehktov@yandex.ru>
2024-05-11 20:58:51 +0300

    ДЗ 2 почти завершено

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
-import styles from './page.module.css';
-import Logo from './logo.svg';
 import PostCard from '@/components/PostCard/PostCard';
-import LikeButton from '@/components/LikeButton/LikeButton';
 
 export default function Home() {
 	return (
-		<div className={styles.main}>
-			<Logo />
+		<>
 			<PostCard />
-			<LikeButton />
-		</div>
+			<PostCard />
+			<PostCard />
+			<PostCard />
+			<PostCard />
+			<PostCard />
+			<PostCard />
+			{/*<LikeButton />*/}
+		</>
 	);
 }
--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -1,13 +1,22 @@
 import styles from './Header.module.css';
 import {HTMLAttributes} from 'react';
+import GithubImage from '@/public/github.svg';
+import LogoCompanyImage from '@/public/LogoCompany.svg';
+import Link from 'next/link';
+import cn from 'classnames';
 
 interface HeaderProps extends HTMLAttributes<HTMLHeadElement> {
 }
 
 export default function Header({className}: HeaderProps) {
 	return (
-		<header className={className}>
-            тут шапка
+		<header className={cn(className, styles.headerContainer)}>
+			<Link href='/'>
+				<LogoCompanyImage className={styles.logo}/>
+			</Link>
+			<Link href='https://github.com/mister-Mandarin/PurpleSchool-NextCource-Blog' target='_blank'>
+				<GithubImage className={styles.logo}/>
+			</Link>
 		</header>
 	);
 }
